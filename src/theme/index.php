0c6aef1505407e4dd602bec55b866ae1f32ad1fe
alshchetinin <alexsander.qw@gmail.com>
2020-10-26 14:46:10 +0300

    add hero block

--- src/theme/index.php
+++ src/theme/index.php
@@ -1,7 +1,66 @@
 <?php get_header(); ?>
 
-<section class="hero">
-   
+<section class="hero" style="background-image: url(<?php echo get_template_directory_uri()?>/img/bg.jpg)">
+   <div class="wrapper">
+      <div class="row">
+         <div class="hero__title">
+            <h1>Интеллектуальная система <span>«Мажордом»</span></h1>
+         </div>
+         <div class="hero__description">
+            Умные дома под ключ: от проекта до приложения
+         </div>         
+         <div class="hero__advantages">
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Видео<br>наблюдение</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Доступ<br>авто</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Оплата<br>ЖКХ</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Чаты<br>с жильцами</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">IP<br>дмомофония</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Дистанционный<br>контроль </div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Заявки<br>в управляющую компанию</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Алиса</div>
+               
+         </div>
+         <div class="advantage">
+               <div class="advantage__icon"><img src="<?php echo get_template_directory_uri()?>/img/icon.svg" alt="" width="37px"></div>
+               <div class="advantage__text">Безключевой<br>доступ</div>
+               
+         </div>                                                                
+         </div>
+         <div class="hero__button">
+            <a href="#" class="button button-primary button-big">Узнать на что способен мажордом</a>
+         </div>
+      </div>
+   </div>
 </section>
 
 <div class="wrapper">
