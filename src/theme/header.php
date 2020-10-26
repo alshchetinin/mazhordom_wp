2ac6be4ff5484e204b206070212096240da8de74
alshchetinin <alexsander.qw@gmail.com>
2020-10-26 12:01:54 +0300

    add debager gridd

--- src/theme/header.php
+++ src/theme/header.php
@@ -7,6 +7,24 @@
 </head>
 
 <body <?php body_class(); ?>>
+<div class="debug-grid">
+   <div class="">
+      <div class="debug-grid__colums">
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+         <div class=""></div>
+      </div>
+   </div>
+</div>
 <header id="header" class="container header">
 	<h1><?php bloginfo( 'name' ); ?></h1>
 </header>
