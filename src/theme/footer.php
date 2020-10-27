bba3928c8a0a459eb7e417af64357a345b0b7d9d
alshchetinin <alexsander.qw@gmail.com>
2020-10-27 14:55:59 +0300

    add blocks

--- src/theme/footer.php
+++ src/theme/footer.php
@@ -1,14 +1,5 @@
 <footer class="container footer">
-	<div class="footer-inner">
-		<div class="text-row">
-			<p>
-				<?php bloginfo( 'name' ); ?>
-			</p>
-			<p>
-				Copyright <?php echo date( 'Y' ); ?> &copy; All rights reserved
-			</p>
-		</div>
-	</div>
+
 </footer>
 <?php wp_footer(); ?>
 </body>
--- src/theme/header.php
+++ src/theme/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1.0"/>
 	<?php wp_head(); ?>
 </head>
 
